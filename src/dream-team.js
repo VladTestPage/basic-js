9a38e5857924650bc4a57ecb08073cf0d927dd15
VladTestPage <kissntrill@gmail.com>
2022-10-15 16:49:28 +0300

    add:task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,71 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(array) {
+  if (array === null || array === undefined) {
+    return false
+  }
+
+  let arrayFiltred = []
+  let arrayFiltredFromSapces = []
+  let i = 0
+  let string = ''
+  let stringSorted = ''
+
+  while (i < array.length) {
+    if (typeof array[i] === 'string') {
+      arrayFiltred.push(array[i])
+    }
+    i++
+  }
+
+  i = 0
+
+  while (i < arrayFiltred.length) {
+    arrayFiltredFromSapces.push(arrayFiltred[i].trim())
+    i++
+  }
+
+  i = 0
+
+  while (i < arrayFiltredFromSapces.length) {
+    string = string + arrayFiltredFromSapces[i].slice(0, 1).toUpperCase()
+    i++
+  }
+
+  stringSorted = string.split('').sort().join('').toUpperCase()
+
+  return stringSorted
 }
 
+console.log(
+  createDreamTeam([
+    ['David Abram'],
+    ['Robin Attfield'],
+    'Thomas Berry',
+    ['Paul R.Ehrlich'],
+    'donna Haraway',
+    ' BrIaN_gOodWiN  ',
+    {
+      0: 'Serenella Iovino',
+    },
+    'Erazim Kohak',
+    '  val_plumwood',
+  ])
+)
+
 module.exports = {
-  createDreamTeam
-};
+  createDreamTeam,
+}
