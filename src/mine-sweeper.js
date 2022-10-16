a15173de3cd181fb6c69ae9c795ac70bdc6d9a59
VladTestPage <kissntrill@gmail.com>
2022-10-17 00:10:41 +0300

    add: task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -27,8 +27,32 @@ function minesweeper(matrix) {
   let matrixCopy = JSON.parse(JSON.stringify(matrix))
   let row = 0
   let column = 0
+  let i = 0
+  let counter = 0
   let temp = []
+  let matrixFlat = matrix.flat()
+  let matrixZero = JSON.parse(JSON.stringify(matrix))
 
+  // CHECKING FOR EMPTY MAP
+  while (row < matrix.length) {
+    while (column < matrix[0].length) {
+      matrixZero[row][column] = 0
+      column++
+    }
+    column = 0
+    row++
+  }
+
+  while (i < matrixFlat.length) {
+    if (matrixFlat[i] === true) {
+      break
+    }
+
+    return matrixZero
+  }
+  // CHEKING END
+
+  // BOMB REPLACER
   while (row < matrix.length) {
     while (column < matrix[0].length) {
       if (matrix[row][column] === true) {
@@ -40,36 +64,96 @@ function minesweeper(matrix) {
     row++
   }
 
-  console.log(matrix)
   column = 0
   row = 0
+
+  // END BOMB REPLACER
+
+  // CHECKING NEIGHBOR SLOTS
 
   while (row < matrix.length) {
     while (column < matrix[0].length) {
       console.log(matrix[row][column], 'row', row, 'column', column)
-      // if (matrix[row + 1][column + 1] === true) {
-      //   matrix[row][column] = 9
-      // }
-      // temp.push(matrix[row][column])
-      if (
-        row > 0 &&
-        row < matrix.length - 1 &&
-        column > 0 &&
-        column < matrix[0].length - 1
-      ) {
-        temp.push(matrix[row - 1][column])
-        temp.push(matrix[row - 1][column + 1])
-        temp.push(matrix[row][column + 1])
-        temp.push(matrix[row + 1][column + 1])
-        temp.push(matrix[row + 1][column])
-        temp.push(matrix[row + 1][column - 1])
-        temp.push(matrix[row][column - 1])
-        temp.push(matrix[row - 1][column - 1])
+
+      if (row === 0) {
+        if (column === 0) {
+          temp.push(matrix[row][column + 1])
+          temp.push(matrix[row + 1][column + 1])
+          temp.push(matrix[row + 1][column])
+        } else if (column === matrix[0].length - 1) {
+          temp.push(matrix[row][column - 1])
+          temp.push(matrix[row + 1][column - 1])
+          temp.push(matrix[row + 1][column])
+        } else if (column > 0 && column < matrix[0].length) {
+          temp.push(matrix[row][column - 1])
+          temp.push(matrix[row + 1][column - 1])
+          temp.push(matrix[row + 1][column])
+          temp.push(matrix[row + 1][column + 1])
+          temp.push(matrix[row][column + 1])
+        }
       }
 
+      if (row === matrix.length - 1) {
+        if (column === 0) {
+          temp.push(matrix[row - 1][column])
+          temp.push(matrix[row - 1][column + 1])
+          temp.push(matrix[row][column + 1])
+        } else if (column === matrix[0].length - 1) {
+          temp.push(matrix[row - 1][column])
+          temp.push(matrix[row - 1][column - 1])
+          temp.push(matrix[row][column - 1])
+        } else if (column > 0 && column < matrix[0].length) {
+          temp.push(matrix[row][column - 1])
+          temp.push(matrix[row - 1][column - 1])
+          temp.push(matrix[row - 1][column])
+          temp.push(matrix[row - 1][column + 1])
+          temp.push(matrix[row][column + 1])
+        }
+      }
+
+      if (row > 0 && row < matrix.length - 1) {
+        if (column === 0) {
+          temp.push(matrix[row - 1][column])
+          temp.push(matrix[row - 1][column + 1])
+          temp.push(matrix[row][column + 1])
+          temp.push(matrix[row + 1][column + 1])
+          temp.push(matrix[row + 1][column])
+        } else if (column === matrix[0].length - 1) {
+          temp.push(matrix[row][column - 1])
+          temp.push(matrix[row - 1][column - 1])
+          temp.push(matrix[row - 1][column])
+          temp.push(matrix[row - 1][column + 1])
+          temp.push(matrix[row][column + 1])
+        } else if (column > 0 && column < matrix[0].length) {
+          temp.push(matrix[row - 1][column])
+          temp.push(matrix[row - 1][column + 1])
+          temp.push(matrix[row][column + 1])
+          temp.push(matrix[row + 1][column + 1])
+          temp.push(matrix[row + 1][column])
+          temp.push(matrix[row + 1][column - 1])
+          temp.push(matrix[row][column - 1])
+          temp.push(matrix[row - 1][column - 1])
+        }
+      }
+      console.log(temp, 'temp', 'row', row, 'column', column)
+
+      while (i < temp.length) {
+        if (temp[i] === true) {
+          counter++
+          matrixCopy[row][column] = counter
+        }
+        console.log(counter, 'counter')
+
+        i++
+      }
+
+      console.log(matrixCopy[row][column], 'counter array')
+      counter = 0
+      i = 0
       column++
+      temp = []
     }
-    console.log(temp, 'temp', 'row', row, 'column', column - 1)
+
     temp = []
     column = 0
     row++
@@ -79,12 +163,11 @@ function minesweeper(matrix) {
 }
 
 const matrix = [
-  [true, false, false],
-  [false, true, false],
+  [false, false, false],
   [false, false, false],
 ]
 
-console.log(minesweeper(matrix))
+console.log(minesweeper(matrix), 'return')
 
 module.exports = {
   minesweeper,
